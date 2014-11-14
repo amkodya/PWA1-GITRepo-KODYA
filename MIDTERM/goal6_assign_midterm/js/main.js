bcaa88ae8a178cd12150fe86b4250045fb9abb8d
amkodya <electrikoala@Ashleys-MacBook-Pro.local>
2014-11-14 17:11:19 -0500

    MIDTERM FINAL
    MIDTERM FINAL

--- MIDTERM/goal6_assign_midterm/js/main.js
+++ MIDTERM/goal6_assign_midterm/js/main.js
@@ -6,20 +6,18 @@
 */
 (function(){
 
+    //array of student 1 variables
 var student1 = {name:'Jerry Garcia',address:{street:'123 Shakedown st',city:'Burlington',state:'VT'},gpa:[3.2, 3.5, 2.9]};
-//var student2 = {name:'Bobby Weir',address:{street:'123 Bertha rd',city:'Buffalo',state:'NY'},gpa:[3.0, 3.2, 3.3]};
-//var student3 = {name:'Phil Lesh',address:{street:'123 Brokedown Palace',city:'New York',state:'NY'},gpa:[2.5, 2.1, 3.0]};
 
-
-console.log('Student name: '+ student1.name);
-console.log('Student Address: '+ student1.address.street+', '+student1.address.city+', '+student1.address.state);
-console.log('Student GPA\'s: '+ student1.gpa[0]+', '+student1.gpa[1]+', '+student1.gpa[2]);
+console.log('Student name: '+ student1.name);          //submits array variable to console
+console.log('Student Address: '+ student1.address.street+', '+student1.address.city+', '+student1.address.state);   //submits array variable to console
+console.log('Student GPA\'s: '+ student1.gpa[0]+', '+student1.gpa[1]+', '+student1.gpa[2]);   //submits array variable to console
 
     var clickButton = document.getElementById('info_btn');   //click object FIGHT box to begin function
-    clickButton.onclick = function addData(){
+    clickButton.onclick = function addData(){                 //function envoked by clicking button
 
 
-
+    //std1 object to display student information in the function addData()
     var std1 = {
         stdName: 'Jerry Garcia',
         stdAddress: '123 Shakedown St, Burlington, VT',
@@ -29,46 +27,58 @@ console.log('Student GPA\'s: '+ student1.gpa[0]+', '+student1.gpa[1]+', '+studen
             {gpa: 2.9}
         ]
 
-
     };
-        console.log('Student Name: ' + std1.stdName);
-        console.log('Student Address: ' + std1.stdAddress);
-        console.log('Student GPA: ' +std1.stdGPA[0].gpa+', '+std1.stdGPA[1].gpa+', '+std1.stdGPA[2].gpa);
+        console.log('Student Name: ' + std1.stdName);                   // student name to the console from object
+        console.log('Student Address: ' + std1.stdAddress);             // student address to the console from object
+        console.log('Student GPA: ' +std1.stdGPA[0].gpa+', '+std1.stdGPA[1].gpa+', '+std1.stdGPA[2].gpa); //student gpa to the console from object
 
 
-        var gradeAvg = function (stu1) {
-            var count = 0;
-            var total = 0;
-            for (var i = 0, j = stu1.stdGPA.length; i < j; i++) {
-                count++;
-                total = total + stu1.stdGPA[i]['gpa'];
+
+
+        var gradeAvg = function (stu1) {                            // gradeAvg function to calculate averages of GPA
+            var count = 0;                                       // starts count at zero
+            var total = 0;                                         //starts total at zero
+            for (var i = 0, j = stu1.stdGPA.length; i < j; i++) {     //for loop
+                count++;                                           //counts number of gpa averages
+                total = total + stu1.stdGPA[i]['gpa'];            //calculates total of gpa averages
 
             };
-            return total / count;
+            return total / count;              // returns the total divided the number of gpa averages
         };
 
 
-        console.log('Average Grade: ', gradeAvg(std1));
+        console.log('Average Grade: ', gradeAvg(std1));   //console log the average of grades to the console
 
 
 
 
+    var std1Name = document.getElementById("name");           // shows name on name link line from html
+    std1Name.innerHTML = std1.stdName;
+
+    var std1Address = document.getElementById("address");        // shows address in address line from html
+    std1Address.innerHTML = std1.stdAddress;                     // shows address in address line
+
+    var std1GPA = document.getElementById("gpa");                                              // shows gpa scores in gpa line
+    std1GPA.innerHTML = std1.stdGPA[0].gpa+', '+std1.stdGPA[1].gpa+', '+std1.stdGPA[2].gpa;   // displays gpa scores in gpa line
+
+    var std1GpaAverage = document.getElementById("gpaavg");         // dom object to show average of gpa from html
+    std1GpaAverage.innerHTML = gradeAvg(std1);                     // calls upon gradeAvg() function
+
+    var noMoreInfo =  disableBtn();           //function to disable button after click
+       console.log(noMoreInfo);
+
+        console.log(window);                 // global dom object
+        console.log(window.location);       // global dom object
+        console.log(window.history);        // global dom object
+        console.log(window.navigator);      // global dom object
 
 
 
 
+           var giveDate = document.getElementById("date");   // shows date at end of object
+            giveDate.innerHTML = Date();                     // date function as given by "console.log(window)"
 
-var std1Name = document.getElementById("name");
-std1Name.innerHTML = std1.stdName;
 
-var std1Address = document.getElementById("address");
-std1Address.innerHTML = std1.stdAddress;
-
-var std1GPA = document.getElementById("gpa");
-std1GPA.innerHTML = std1.stdGPA[0].gpa+', '+std1.stdGPA[1].gpa+', '+std1.stdGPA[2].gpa;
-
-var std1GpaAverage = document.getElementById("gpaavg");
-std1GpaAverage.innerHTML = gradeAvg(std1);
 
 
 
@@ -77,7 +87,32 @@ std1GpaAverage.innerHTML = gradeAvg(std1);
 
     };
 
-    document.getElementById("info_btn").disabled = true;
+
+
+
+
+    function disableBtn(){
+
+        var noMoreInfo = student1.gpa.length;
+
+        if (noMoreInfo === 3){
+            var doneMsg = document.querySelectorAll('#info_box a');
+            console.log(doneMsg[1]);
+
+            for (var i= 0, max = doneMsg.length; i<max; i++){
+
+                doneMsg[i].innerHTML = 'Done!!!';
+            };
+            document.getElementById('info_btn').style.pointerEvents = 'none';
+        }else{
+
+            document.getElementById('info_btn').style.pointerEvents = 'auto';
+        };
+
+        return noMoreInfo;
+
+    }
+
 
 //addData();
 
